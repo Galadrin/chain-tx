24a092650c7d50592a2935505dc036056df311f0
David <david.pierret@gmail.com>
2021-01-27 15:09:47 +0100

    Add some code comments in transaction.py

--- transaction.py
+++ transaction.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from dbus import SystemBus, Interface
-from dbus.exceptions import DBusException
 import time
 
 import logging
@@ -35,68 +33,24 @@ class Envoi ():
             gas=200000,
             sync_mode="sync"
         )
-        tx.add_transfer(to_address=self.address_1, amount=amount)
+        tx.add_transfer(to_address=self.address_1, amount=amount, base_denom="basetcro")
         signed_tx = tx.get_pushable()
         return signed_tx
 
-def is_service_running(service):
-    """ Queries systemd through dbus to see if the service is running """
-    service_running = False
-    bus = SystemBus()
-    systemd = bus.get_object('org.freedesktop.systemd1', '/org/freedesktop/systemd1')
-    manager = Interface(systemd, dbus_interface='org.freedesktop.systemd1.Manager')
-    try:
-        service_unit = service if service.endswith('.service') else manager.GetUnit('{0}.service'.format(service))
-        service_proxy = bus.get_object('org.freedesktop.systemd1', str(service_unit))
-        service_properties = Interface(service_proxy, dbus_interface='org.freedesktop.DBus.Properties')
-        service_load_state = service_properties.Get('org.freedesktop.systemd1.Unit', 'LoadState')
-        service_active_state = service_properties.Get('org.freedesktop.systemd1.Unit', 'ActiveState')
-        if service_load_state == 'loaded' and service_active_state == 'active':
-            service_running = True
-    except DBusException:
-        pass
-
-    return service_running
-
-def is_service_active(service):
-    """
-    is_service_active method will check if service is running or not.
-    It raise exception if there is service is not loaded
-    Return value, True if service is running otherwise False.
-    :param str service: name of the service
-    """
-
-    bus = SystemBus()
-    systemd = bus.get_object('org.freedesktop.systemd1', '/org/freedesktop/systemd1')
-    manager = dbus.Interface(systemd, 'org.freedesktop.systemd1.Manager')
-
-    try:
-        manager.GetUnit('chain-maind.service')
-        return True
-    except:
-        return False
-
-def restart_daemon():
-    cmd = f"systemctl restart chain-maind"
-    print("### Restart service ###")
-    sysbus = SystemBus()
-    systemd1 = sysbus.get_object('org.freedesktop.systemd1', '/org/freedesktop/systemd1')
-    manager = dbus.Interface(systemd1, dbus_interface='org.freedesktop.systemd1.Manager')
-    if is_service_active('chain-maind.service'):
-        manager.StopUnit('chain-maind.service', 'replace')
-     #job = manager.RestartUnit('chain-maind.service', 'fail')
-    time.sleep(10)
-    manager.StartUnit('chain-maind.service', 'replace')
-    exit("Chain-maind restart")
-
 def main():
     print("### STARTING FLODDING ###")
+    
+    # create a wallet from my seed
     seed = "warrior sponsor tiger lift ship clog shrimp rent critic pony isolate clever lake notable gas enlist photo whisper excite toy master future van universe"
     wallet_1 = Wallet(seed)
     address_1 = wallet_1.address
     print(address_1)
     
+    # define my delegator address
     delegatorAddr = "crocncl13njqv0la9cw2mr80utsmeppmtxk57kfggpnhly"
+    
+    # define all the endpoint we can need
+    
     # the api port setted in ${home_dir of chain-maind}/config/app.toml, the default is ~/.chain-maind/config/app.toml
     crypto_url = "https://crossfire-lcd.crypto.com/"
     local_url = "http://localhost:1317"
@@ -108,14 +62,16 @@ def main():
     url_balance = f"{local_url}/cosmos/bank/v1beta1/balances/{address_1}"
     url_withdraw = f"{local_url}/distribution/delegators/{delegatorAddr}/rewards"
 
-
+    # init request session to reuse tcp socket
     r = requests.Session()
     response = r.get(url_account)
     account_info = response.json()["account"]
     account_num = int(account_info["account_number"])
     sequence = int(account_info["sequence"])
-
+    
+    # create transaction object
     send_object = Envoi(wallet_1, account_num)
+    
     #create a logger
     logger = logging.getLogger('mylogger')
 
@@ -128,6 +84,8 @@ def main():
         # get the balance of address_1
         response = r.get(url_balance)
         balance_1 = int(response.json()["balances"][0]["amount"])
+        
+        # get account number and sequence ID
         response = r.get(url_account)
         account_info = response.json()["account"]
         account_num = int(account_info["account_number"])
@@ -137,7 +95,9 @@ def main():
         print(f"retrieve sequence num : {sequence}")
         print(f"balance of address 1: {balance_1}")
 
+        # check the balance is enought to not drain all our tcro
         if balance_1 >= 4000000 :
+            # define last sequence number for the loop
             stop_at = sequence + 300
             seq = sequence
             for seq in range(sequence, stop_at):
@@ -145,6 +105,7 @@ def main():
                 # make transaction
                 signed_tx = send_object.get_pushTx_sync(amount=1, sequence=seq)
                 try:
+                    # send the Tx
                     response = r.post(url_tx, json=signed_tx)
                     response.raise_for_status()
                 except http_error as e:
@@ -154,11 +115,13 @@ def main():
 
                 result = response.json()
                 code = result.get("code")
+                # error code managment
                 if not code:
                     print(response.text)
                     continue
                 if code == 0:
                     print(f"error {code} tx OK")
+                    # increment the sequence number
                     seq = seq + 1
                 elif  code == 4:
                     print(f"sequence {seq} error {code} unauthorized")
@@ -169,16 +132,20 @@ def main():
                     time.sleep(5)
                     break
                 elif  code == 19:
+                    # the Tx already exist, sequence number not up to date
                     print(f"error {code} tx {seq} already in mempool", end='\n')
                     break
                 elif  code == 20:
+                    # mempool is full, give time to purge
                     print(f"index {seq} error {code} mempool is full")
                     time.sleep(1)
                     break
                 elif  code == 32:
-                    print(f"index {seq} error {code} sequence error (congestion)")
+                    print(f"index {seq} error {code} sequence error (congestion?)")
                 else :
+                    # any other code
                     print(f"index {seq} error {code} ")
+            # wait some time => if not, we have more often error 4
             time.sleep(5)
             # end for loop
         else:
